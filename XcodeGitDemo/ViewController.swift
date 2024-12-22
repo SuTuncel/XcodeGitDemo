6e8837b096181a4bbe4b358bb418f1a76e9e07a2
Su Tuncel <83240777+SuTuncel@users.noreply.github.com>
2024-12-22 16:35:34 +0300

    Initial Commit

---
+++ XcodeGitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeGitDemo
+//
+//  Created by Elif Su Tuncel on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
